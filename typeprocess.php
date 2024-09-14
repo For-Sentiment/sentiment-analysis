1ba3ea241aea394fefac846760f9854e6ab8afa0
For-Sentiment <salvadormarkcarmelo@gmail.com>
2024-09-14 12:43:16 +0800

    Update typeprocess.php

--- typeprocess.php
+++ typeprocess.php
@@ -5,7 +5,6 @@ $comment = $_POST['comment'];
 // Use cURL to send the comment to the Flask API and get the result
 $api_url = 'https://nlp-sentiment-analysis-f4u4.onrender.com';
 
-
 $ch = curl_init($api_url);
 $data = json_encode(array('comment' => $comment));
 
@@ -14,12 +13,22 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'cURL Error: ' . curl_error($ch);
+    curl_close($ch);
+    exit();
+}
 curl_close($ch);
 
 // Decode the API response
 $sentiment_data = json_decode($result, true);
-$sentiment = $sentiment_data['sentiment'];
-$cleaned_comment = $sentiment_data['comment'];
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo 'JSON Decode Error: ' . json_last_error_msg();
+    exit();
+}
+
+$sentiment = isset($sentiment_data['sentiment']) ? $sentiment_data['sentiment'] : 'unknown';
+$cleaned_comment = isset($sentiment_data['comment']) ? $sentiment_data['comment'] : 'No comment';
 
 // Redirect to result.php with sentiment data
 header('Location: Result.php?sentiment=' . urlencode($sentiment) . '&comment=' . urlencode($cleaned_comment));
